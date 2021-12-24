a2066319dfa2a39589f69d1edcca6f8871d6b2d3
NL <dindin65720@gamil.com>
2021-12-24 13:08:36 +0630

    more questions

--- quizz-game.py
+++ quizz-game.py
@@ -4,14 +4,57 @@ play = input("Start the game? yes/no? ")
 
 if play != "yes":
     quit()
-    
-print("Loading the game...")
+
+rules = ('Rules: type answers with no Cap and space between each word')
+print(rules) 
+print("The game has started")
+
+a1 = str(['central processing unit'])
+a2 = str(['graphics processing unit'])
+a3 = str(['random access memory'])
+a4 = str(['read only memory'])
+a5 = str(['hard disk drive'])
+a6 = str(['solid state drive'])
+a7 = str(['universal serial bus'])
 
 user_answer = input("What does CPU stand for? ")
-answer = ['central processing unit', "Central Processing Unit", "centralprocessingunit", "CentralProcessingUnit"]
-str(answer)
-
-if user_answer in answer:
+if user_answer in a1:
     print("correct!")    
 else:
-    print("incorrect")
+    print("incorrect!")
+
+user_answer = input("What does GPU stand for? ")
+if user_answer in a2:
+    print("correct!")    
+else:
+    print("incorrect!")
+
+user_answer = input("What does RAM stand for? ")
+if user_answer in a3:
+    print("correct!")    
+else:
+    print("incorrect!")
+  
+user_answer = input("What does ROM stand for? ")  
+if user_answer in a4:
+    print("correct!")    
+else:
+    print("incorrect!")
+
+user_answer = input("What does HDD stand for? ")    
+if user_answer in a5:
+    print("correct!")    
+else:
+    print("incorrect!")
+
+user_answer = input("What does SSD stand for? ")    
+if user_answer in a6:
+    print("correct!")    
+else:
+    print("incorrect!")
+
+user_answer = input("What does USB stand for? ")    
+if user_answer in a7:
+    print("correct!")    
+else:
+    print("incorrect!")
