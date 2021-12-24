e467b2d4ec550172ec03ae5cd346377b67a98916
NL <dindin65720@gamil.com>
2021-12-24 15:44:35 +0630

    answer sys & score tracking

--- quizz-game.py
+++ quizz-game.py
@@ -1,4 +1,4 @@
-print("Welcome to Quizz Game")
+print("Welcome to ICT Quizz Game")
 
 play = input("Start the game? yes/no? ")
 
@@ -9,52 +9,57 @@ rules = ('Rules: type answers with no Cap and space between each word')
 print(rules) 
 print("The game has started")
 
-a1 = str(['central processing unit'])
-a2 = str(['graphics processing unit'])
-a3 = str(['random access memory'])
-a4 = str(['read only memory'])
-a5 = str(['hard disk drive'])
-a6 = str(['solid state drive'])
-a7 = str(['universal serial bus'])
+mark = 0
 
 user_answer = input("What does CPU stand for? ")
-if user_answer in a1:
-    print("correct!")    
+if user_answer == 'central processing unit':
+    print("correct!")
+    mark += 1    
 else:
     print("incorrect!")
 
 user_answer = input("What does GPU stand for? ")
-if user_answer in a2:
-    print("correct!")    
+if user_answer == 'graphics processing unit':
+    print("correct!")
+    mark += 1    
 else:
     print("incorrect!")
 
 user_answer = input("What does RAM stand for? ")
-if user_answer in a3:
-    print("correct!")    
+if user_answer == 'random access memory':
+    print("correct!")
+    mark += 1    
 else:
     print("incorrect!")
   
 user_answer = input("What does ROM stand for? ")  
-if user_answer in a4:
-    print("correct!")    
+if user_answer == 'read only memory':
+    print("correct!")
+    mark += 1    
 else:
     print("incorrect!")
 
 user_answer = input("What does HDD stand for? ")    
-if user_answer in a5:
-    print("correct!")    
+if user_answer == 'hard disk drive':
+    print("correct!")
+    mark += 1    
 else:
     print("incorrect!")
 
 user_answer = input("What does SSD stand for? ")    
-if user_answer in a6:
-    print("correct!")    
+if user_answer == 'solid state drive':
+    print("correct!")
+    mark += 1    
 else:
     print("incorrect!")
 
 user_answer = input("What does USB stand for? ")    
-if user_answer in a7:
-    print("correct!")    
+if user_answer == 'universal serial bus':
+    print("correct!")
+    mark += 1    
 else:
     print("incorrect!")
+
+total_question = 7
+print("Total score: " + str(mark) + "/7")
+print("Percentage: " + str(round((mark/total_question)*100)) + "%")
